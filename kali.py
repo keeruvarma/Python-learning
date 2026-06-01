bb0cafb493f2313d72fd194fa6449d2a8c2b9eb5
keeruvarma <145831191+keeruvarma@users.noreply.github.com>
2026-06-01 21:42:39 +0530

    Day1 python basics

---
+++ kali.py
@@ -0,0 +1,6 @@
+#print("My self a women who working very hard since", 5, "years god should help me")
+"""
+print("you will get high package surely 14lpa")
+hey hello nee check this , if you have any doubt do let me know
+ """
+print("hello")
