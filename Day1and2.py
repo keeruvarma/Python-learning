153f03e7e0cd3d7ef1f2a5bdf1fccd537f7c47f0
keeruvarma <145831191+keeruvarma@users.noreply.github.com>
2026-06-03 22:40:36 +0530

    Day1 and 2 practiced problems

---
+++ Day1and2.py
@@ -0,0 +1,56 @@
+# print("My name is keerthana,I am 25 from Hyderabad")
+"""sum of numbers"""
+# a=2
+# b=4
+# sum=a+b
+# z=print("sum of two",sum)
+"""store price and quantity"""
+# mprice = 10
+# quantity = 2
+# total = mprice * quantity
+# print("total price ",total)
+"""swap haha just tried"""
+# a=5
+# b=10
+# if a==b and b==a:
+#     print(b,a)
+"""Difference"""
+# C_slary=6.5
+# E_salary=20
+# F_salary=(C_slary - E_salary)
+# print("y future slary",F_salary)
+# age=20
+# if age>=18:
+#     print("Greater age",age)
+# else:
+#     print("Less than age")
+# a=int(input("Enter the number: "))
+# b=int(input("Enter the another number: "))
+# if a>10:
+#     print("a and b is greater")
+# elif a<10:
+#     print("a is less value")
+# else:
+#     print("a and b values are less")
+"""take a numver even or odd"""
+# a = int(input("Enter the number: "))
+# #b = int(input("Enter any another number: "))
+# if a%2==0:
+#     print("Even number")
+# else:
+#     print("odd number")
+"""marks"""
+# marks=int(input("Enter the marks= "))
+# if marks>=90:
+#     print("Grade A")
+# elif marks>=75:
+#     print("Grade B")
+# else:
+#     print("Grade c")
+"""Login sucess"""
+name=str(input("Enter username:"))
+PWD=str(input("Enter password:"))
+if PWD == "keeru":
+    print("Login success")
+else:
+    print("Login failed")
