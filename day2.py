021fe63212657c2ed3288541bf1d204a9afaf530
keeruvarma <145831191+keeruvarma@users.noreply.github.com>
2026-06-03 09:08:15 +0530

    Day2 python progress

---
+++ day2.py
@@ -0,0 +1,9 @@
+#name = input("Enter your name: ")
+#print("Hello",name)
+"""By using if calling name"""
+#name = input("Enter you name: ")
+# if name =="keerthana":
+#     print("welcome back",name)
+# else:
+#     print("Hello",name)
+    
