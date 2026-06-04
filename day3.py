fe259882f32974946a1d36561ad24a659c17d901
keeruvarma <145831191+keeruvarma@users.noreply.github.com>
2026-06-04 13:42:57 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,38 @@
+# for i in range(1,4):
+#     print(i)
+# i = 1
+# while i<=5:
+#     print(i)
+#     i += 1
+""" numbers from 1-10"""
+# for a in range(1,11):
+#     print(a)
+"""Even nunbers"""
+# i=1
+# while i<=20:
+#     if i%2==0:
+#         print(i)
+#     i+=1
+"""Reverse numbers"""
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+"""multiplication"""
+# i=2
+# while i<=10:
+#     if i*2:
+#         print(i)
+#     i+=1
+""" 1-10"""
+# for i in range(1,10):
+#     print(i)
+"""even"""
+# for i in range(3):
+#     print("Retrying...",i+1)
+success = False
+for i in range(1,3):
+    print("Retrying...")
+    if success:
+        print("success")
+    break 
